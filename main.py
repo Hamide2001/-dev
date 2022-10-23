a23ea67508c078a2a1fa143a0c335ab5c0d23a88
Hamide2001 <hamide.ulusy@icloud.com>
2022-10-23 16:19:51 +0300

    Ödev.

--- main.py
+++ main.py
@@ -1,79 +1,32 @@
-menu = {'kola':10,
-       'tuz':40,
-       'pasta':50,
-       'kek':25,
-       'soda' :8,
-       'defter':35, 
-       'su' :3,
-       'sut':10,
-       'cay':100, 
-       'sabun':16}
+vize = input("Vize:")
+final = input("Final:")
+puan = int(vize)*0.4+int(final)*0.6
 
-print("----Menu su sekildedir----")
-for i in menu:
-    print(i)
+if puan >= 90:
+    print("AA")
 
-almak_istenen = input("Ne almak istersiniz? : ")
+if puan >= 85:
+    print("BA")
 
-if(almak_istenen == 'kola'):
-    print("Hesap: ",menu['kola'])
-    para = int(input("Para giriniz: "))
-    kalan = para - menu['kola']
-    print("Kalan para: ",kalan)
+elif puan >= 80:
+    print("BB")
 
-elif(almak_istenen == 'tuz'):
-    print("Hesap: ",menu['tuz'])
-    para = int(input("Para giriniz: "))
-    kalan = para-menu['tuz']
-    print("Kalan para: ",kalan)
+elif puan >= 75:
+    print("CB")
 
-elif(almak_istenen == 'pasta'):
-    print("Hesap: ", menu['pasta'])
-    para = int(input("Para giriniz: "))
-    kalan = para-menu['pasta']
-    print("Kalan para: ",kalan)
+elif puan >= 70:
+    print("CC")
 
-elif(almak_istenen == 'kek'):
-    print("Hesap: ", menu['kek'])
-    para = int(input("Para giriniz: "))
-    kalan = para-menu['kek']
-    print("Kalan para: ",kalan)
+elif puan >= 65:
+    print("DC")
 
-elif(almak_istenen == 'soda'):
-    print("Hesap: ", menu['soda'])
-    para = int(input("Para giriniz: "))
-    kalan = para-menu['soda']
-    print("Kalan para: ",kalan)
-
-elif(almak_istenen == 'defter'):
-    print("Hesap: ",menu['defter'])
-    para = int(input("Para giriniz: "))
-    kalan = para-menu['defter']
-    print("Kalan para: ",kalan)
-
-elif(almak_istenen == 'su'):
-    print("Hesap: ", menu['su'])
-    para = int(input("Para giriniz: "))
-    kalan = para-menu['su']
-    print("Kalan para: ",kalan)
-
-elif(almak_istenen == 'sut'):
-    print("Hesap: ", menu['sut'])
-    para = int(input("Para giriniz: "))
-    kalan = para-menu['sut']
-    print("Kalan para: ",kalan)
-
-elif(almak_istenen == 'cay'):
-    print("Hesap: ", menu['cay'])
-    para = int(input("Para giriniz: "))
-    kalan = para-menu['cay']
-    print("Kalan para: ",kalan)
-
-elif(almak_istenen == menu['sabun']):
-    print("Hesap: ",menu['sabun'])
-    para = int(input("Para giriniz: "))
-    kalan = para-menu['sabun']
-    print("Kalan para: ",kalan)
+elif puan >= 60:
+    print("DD")
 
 else:
-    print("Hesap 0. Girdiginiz urun marketimizde bulunmamaktadir.")
+    print("FF")
+
+
+
+
+
